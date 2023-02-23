fcfb667d86e3b35957da4c4baaa3ac4160f9183d
Air128731 <henry.scott0x8080@gmail.com>
2023-02-23 06:03:28 -0500

    first push

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema for Users
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        default: Date.now,
+    },
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
